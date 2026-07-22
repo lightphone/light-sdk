faa5b6f78f295190a4c422289594c8ce23186fe2
Gustavo Brunoro <git@hitnail.net>
2026-07-22 18:58:11 -0300

    fix: progressbar ui example has back button

--- examples/ui-demo/src/main/kotlin/com/thelightphone/uidemo/UiDemoProgressBarScreen.kt
+++ examples/ui-demo/src/main/kotlin/com/thelightphone/uidemo/UiDemoProgressBarScreen.kt
@@ -1,10 +1,8 @@
 package com.thelightphone.uidemo
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -14,13 +12,16 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.thelightphone.sdk.SealedLightActivity
 import com.thelightphone.sdk.SimpleLightScreen
+import com.thelightphone.sdk.ui.LightBarButton
+import com.thelightphone.sdk.ui.LightIcons
 import com.thelightphone.sdk.ui.LightText
 import com.thelightphone.sdk.ui.LightTextVariant
 import com.thelightphone.sdk.ui.LightTheme
 import com.thelightphone.sdk.ui.LightThemeController
+import com.thelightphone.sdk.ui.LightTopBar
+import com.thelightphone.sdk.ui.LightTopBarCenter
 import com.thelightphone.sdk.ui.LightTouchableProgressBar
 import com.thelightphone.sdk.ui.gridUnitsAsDp
 import java.util.Locale
@@ -29,22 +30,32 @@ class UiDemoProgressBarScreen(sealedActivity: SealedLightActivity) :
     SimpleLightScreen<Unit>(sealedActivity) {
 
     @Composable
-    override fun Content() = ContentInner()
+    override fun Content() = ContentInner(onBack = { goBack() })
 }
 
 @Composable
-private fun ContentInner() {
+private fun ContentInner(onBack: () -> Unit) {
     val themeColors by LightThemeController.colors.collectAsState()
     var progress by remember { mutableStateOf(0.5f) }
     LightTheme(colors = themeColors) {
-        Column(
-            horizontalAlignment = CenterHorizontally,
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(2f.gridUnitsAsDp())
-        ) {
-            LightText("%.2f".format(Locale.ROOT, progress), variant = LightTextVariant.Copy)
-            LightTouchableProgressBar(themeColors, progress, onValueChange = { progress = it })
+        Column(modifier = Modifier.fillMaxSize()) {
+            LightTopBar(
+                leftButton = LightBarButton.LightIcon(
+                    icon = LightIcons.BACK,
+                    onClick = onBack,
+                ),
+                center = LightTopBarCenter.Text("Progress Bar"),
+                modifier = Modifier.padding(bottom = 1f.gridUnitsAsDp()),
+            )
+            Column(
+                horizontalAlignment = CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(2f.gridUnitsAsDp())
+            ) {
+                LightText("%.2f".format(Locale.ROOT, progress), variant = LightTextVariant.Copy)
+                LightTouchableProgressBar(themeColors, progress, onValueChange = { progress = it })
+            }
         }
     }
 }
@@ -52,5 +63,5 @@ private fun ContentInner() {
 @Preview(widthDp = 1080 / 3, heightDp = 1240 / 3, showBackground = true)
 @Composable
 fun UiDemoProgressBarScreen() {
-    ContentInner()
+    ContentInner(onBack = {})
 }
